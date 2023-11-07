f7bce1a6bd1a54a6fdd6691926869fa50854fb75
Catalyst <sawantaditya350@gmail.com>
2023-11-07 14:41:06 +0530

    Implemented student CRUD functionality, including create, read, update, and delete operations in the student management module.

---
+++ src/test/java/com/example/demo/AdityaCantroller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdityaCantroller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
